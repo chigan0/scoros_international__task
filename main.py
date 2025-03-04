39fb883412531d3df27d0d1d9491960d0d5410b0
chigan0 <66748549+chigan0@users.noreply.github.com>
2025-03-04 16:35:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,31 @@ def main():
 
 		line1 = file1.readline().strip()
 		line2 = file2.readline().strip()
-	
+
+		prev_line1 = None
+		prev_line2 = None
+
 		while line1 or line2:
-			if not line2 or (line1 and line1 < line2):
-				out1.write(line1 + "\n")  # Write down in output1.txt
+			if line1 and line1 == prev_line1:
 				line1 = file1.readline().strip()
-			
-			elif not line1 or (line2 and line1 > line2):
-				out2.write(line2 + "\n")  # Write down in output2.txt
+				continue
+			if line2 and line2 == prev_line2:
 				line2 = file2.readline().strip()
-			
-			else:  # line1 == line2
+				continue
+
+			if not line2 or (line1 and line1 < line2):
+				out1.write(line1 + "\n")
+				prev_line1 = line1
+				line1 = file1.readline().strip()
+
+			elif not line1 or (line2 and line1 > line2):
+				out2.write(line2 + "\n")
+				prev_line2 = line2
+				line2 = file2.readline().strip()
+
+			else:  # line1 == line2 (совпадает в обоих файлах, не записываем)
+				prev_line1 = line1
+				prev_line2 = line2
 				line1 = file1.readline().strip()
 				line2 = file2.readline().strip()
 
